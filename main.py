352b0b06b6bab8fae4d3eb3215df42960c0a06f4
kihara <kkh20434@gmail.com>
2021-01-17 11:38:15 +0900

    add print_hello

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+def_print_hello():
+    print('hello')
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
